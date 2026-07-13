0b5203865b563785c1960cae74b7c427a7994283
Oliver Rich-Jackson <10Oliver28@gmail.com>
2026-07-13 13:01:26 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import customtkinter
+
+
+## Functional
+
+originWindow = customtkinter.CTk()
+originWindow.geometry("600x500")
+originWindow.title("Poker Trainer")
+
+originWindow.mainloop()
+
+## UI
+
